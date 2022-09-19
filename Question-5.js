fe1d2f4ee317b045147129de4193df0c0728af6f
PSC Student <psc@billkuker.com>
2022-09-19 11:15:14 -0400

    completed up to question 5

--- Question-5.js
+++ Question-5.js
@@ -1,9 +1,13 @@
 
-function countToN(n){
-    //Use a while loop to count to ten, using console.log to print what number you are on
-    // (YOUR CODE HERE)
+function countToN(){
+    let x= 1;
+    while (x < 6) {
+        console.log ("Counting n", x);
+        x++;
+    }
 }
 
 countToN(3);
 
 countToN(5);
+
